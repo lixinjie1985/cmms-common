98a6d95bf87e00e01eee86cf3e4cdd8f9ac51ed2
DELL <DELL@DESKTOP-TR4OCM5>
2017-12-18 11:11:44 +0800

    init

---
+++ src/main/java/com/cmos/ha/cmms/common/App.java
@@ -0,0 +1,13 @@
+package com.cmos.ha.cmms.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
